e648dc6e683d7d3cc0f3360a150cd09f94b0d518
nangtural02 <nangtural02@gmail.com>
2026-05-29 11:18:11 +0900

    style(eyedropper): 스포이드 커서 크기 24→32px, hotspot 4,28로 재조정

--- ui_engine/towa-app/src/components/editor/EyedropperHandler.vue
+++ ui_engine/towa-app/src/components/editor/EyedropperHandler.vue
@@ -233,6 +233,6 @@ onBeforeUnmount(() => {
    hotspot (3,21)은 스포이드 팁(좌하단)에 맞춤. fallback crosshair. */
 .eyedropper-cursor,
 .eyedropper-cursor * {
-  cursor: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'><path d='m2 22 1-1h3l9-9'/><path d='M3 21v-3l9-9'/><path d='m15 6 3.4-3.4a2.1 2.1 0 1 1 3 3L18 9l.4.4a2.1 2.1 0 1 1-3 3l-3.8-3.8a2.1 2.1 0 1 1 3-3l.4.4Z'/><g stroke='black' stroke-width='1.6'><path d='m2 22 1-1h3l9-9'/><path d='M3 21v-3l9-9'/><path d='m15 6 3.4-3.4a2.1 2.1 0 1 1 3 3L18 9l.4.4a2.1 2.1 0 1 1-3 3l-3.8-3.8a2.1 2.1 0 1 1 3-3l.4.4Z'/></g></svg>") 3 21, crosshair !important;
+  cursor: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'><path d='m2 22 1-1h3l9-9'/><path d='M3 21v-3l9-9'/><path d='m15 6 3.4-3.4a2.1 2.1 0 1 1 3 3L18 9l.4.4a2.1 2.1 0 1 1-3 3l-3.8-3.8a2.1 2.1 0 1 1 3-3l.4.4Z'/><g stroke='black' stroke-width='1.6'><path d='m2 22 1-1h3l9-9'/><path d='M3 21v-3l9-9'/><path d='m15 6 3.4-3.4a2.1 2.1 0 1 1 3 3L18 9l.4.4a2.1 2.1 0 1 1-3 3l-3.8-3.8a2.1 2.1 0 1 1 3-3l.4.4Z'/></g></svg>") 4 28, crosshair !important;
 }
 </style>
